5b3338daa691c59a8a87a7ede3f3773b0e079af2
Kwan Yeob Jung <ky@Kwans-MacBook-Pro.local>
2025-04-08 20:06:16 -0400

    Swift project

---
+++ feature_ky/feature_ky/Model/Main.swift
@@ -0,0 +1,8 @@
+//
+//  Main.swift
+//  feature_ky
+//
+//  Created by Kwan Yeob Jung on 2025-04-08.
+//
+
+import Foundation
---
+++ feature_ky/feature_ky/Model/Quiz.swift
@@ -0,0 +1,8 @@
+//
+//  Quiz.swift
+//  feature_ky
+//
+//  Created by Kwan Yeob Jung on 2025-04-08.
+//
+
+import Foundation
