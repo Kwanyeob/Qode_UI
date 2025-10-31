60d8cc882669f8dfcfb5b3c93920d26acc84a183
Kwanyeob <kyeobj@gmail.com>
2025-10-30 22:51:17 -0400

    Finishing off the main page with many placeholders. Need to connect with real images and link all of the pages too

--- Qode/Qode/MVP/Views/Home_pivot/HomeMain.swift
+++ Qode/Qode/MVP/Views/Home_pivot/HomeMain.swift
@@ -7,21 +7,30 @@
 
 import SwiftUI
 
+enum subTitleType {
+    case time
+    case solved
+    
+}
+
 struct HomeMain: View {
     var body: some View {
         ScrollView{
-            VStack{
+            VStack(spacing: 20){
                 HomeHeaderView()
-                Spacer()
                 WeeklyStreakView()
-                Spacer()
-    //            MiniStatView()
-    //            UpNextView()
-    //            WeeklyRecommendView()
-    //            HomeFooterView()
-            }.padding()
+                MiniStatView()
+                UpNextView()
+                WeeklyRecommendView()
+            }.padding(.horizontal)
         // find out a better color. It is too gray. Make it more whiter.
         }.background(Color(.systemGray6))
+        TabView{
+            HomeMenuView(label:"Home", image:"house")
+            HomeMenuView(label:"Roadmap", image:"map")
+            HomeMenuView(label:"Stats", image:"align.vertical.bottom.fill")
+            HomeMenuView(label:"Profile", image:"person.crop.circle")
+        }.frame(height: 45)
     }
 }
 
@@ -61,7 +70,8 @@ struct WeeklyStreakView: View{
                 CheckMarkAndDate(checked:false, inputDay:"Saturday")
                 Spacer()
                 CheckMarkAndDate(checked:false, inputDay:"Sunday")
-            }.padding()
+            }.padding(.horizontal, 8)
+                .padding(.bottom, 8)
         }.padding()
         .background(
             Color.white
@@ -102,13 +112,119 @@ struct CheckMarkAndDate: View {
     }
 }
 
-struct MiniStatCardView: View {
+struct MiniStatView: View {
     var body: some View {
-        Text("Hello, World!")
+        ScrollView(.horizontal,showsIndicators: false){
+            HStack{
+                MiniStatCardView(unit: "DAYS", time: "24", numberSolved: "", title: "Log In Streak", subTitleType: .time)
+                MiniStatCardView(unit: "WEEKS", time: "3", numberSolved: "", title: "Roadmap Progress", subTitleType: .time)
+                MiniStatCardView(unit: "QUIZZES", time: "", numberSolved: "15", title: "# of CS Quizzes Solved", subTitleType: .solved)
+                MiniStatCardView(unit: "solved", time: "", numberSolved: "21", title: "# of Algorithm Solved", subTitleType: .solved)
+                MiniStatCardView(unit: "DAYS", time: "5", numberSolved: "", title: "Average Roadmap\rcompletion Time", subTitleType: .time)
+            }.frame(maxWidth: .infinity, alignment: .leading)
+            
+        }
     }
 }
 
+struct MiniStatCardView: View {
+    let unit : String // hour? week? days?
+    let time : String // If I pass 1 for time and hour for timeFrame, it will be 1 hour.
+    let numberSolved : String
+    let title : String // Log In Streak
+    let subTitleType : subTitleType
 
+    
+    var body: some View {
+        
+        ZStack {
+            Rectangle()
+                .fill(Color.white)
+                .frame(width: 180, height: 150)
+            
+            VStack {
+                Text(title)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.orange)
+                var subTitle: String {
+                    if subTitleType == .solved {
+                        return numberSolved + " " + unit
+                    } else {
+                        return time + " " + unit
+                    }
+                }
+                Spacer()
+                Text(subTitle)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .foregroundColor(.blue)
+            }.padding()
+        }
+        
+    }
+}
+
+// think how to dynamically change the progress and the notification
+struct UpNextView : View {
+    var body: some View{
+        VStack{
+            Text ("UP NEXT")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title2)
+            ZStack{
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: .infinity, height: 150)
+                // add "Complete your js quiz
+                // estimateed time 10 min
+                // think about how this caption can be added dynamically.
+            }
+            Spacer()
+            HStack{
+                Text ("YOUR PROGRESS THIS WEEK").font(.caption).frame(alignment: .leading)
+                Text ("50%").font(.caption).frame(alignment: .trailing)
+            }
+            ProgressView(value: 0.5).frame(maxWidth: .infinity)
+            
+        }
+    }
+}
+
+struct WeeklyRecommendView : View {
+    var body: some View {
+        VStack {
+            Text ("WEEKLY RECOMMENDED")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.title2)
+            ZStack{
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: .infinity, height: 150)
+                // add "Complete your js quiz
+                // estimateed time 10 min
+                // think about how this caption can be added dynamically.
+            }
+            ZStack{
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: .infinity, height: 150)
+                // add "Complete your js quiz
+                // estimateed time 10 min
+                // think about how this caption can be added dynamically.
+            }
+        }
+    }
+}
+
+struct HomeMenuView : View {
+    let label : String
+    let image : String
+    var body: some View {
+        Text("")
+            .tabItem { Label(label, systemImage: image) }
+    }
+}
 
 #Preview {
     HomeMain()
